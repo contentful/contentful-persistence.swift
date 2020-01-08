87f1034e230b11f7d1fcd8bf29a13e26cc1c35b6
jroehl <johann.roehl@contentful.com>
2020-01-08 12:06:22 +0100

    Fix release script

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -8,12 +8,15 @@ git tag $CONTENTFUL_PERSISTENCE_VERSION
 git push --tags
 bundle exec pod trunk push ContentfulPersistenceSwift.podspec --allow-warnings
 make carthage
+git stash --all
 git checkout gh-pages
 git rebase master
 ./Scripts/reference-docs.sh
 git add .
 git commit --amend --no-edit
 git push -f
+git checkout master
+git stash pop
 
 echo "ContentfulPersistence v$CONTENTFUL_PERSISTENCE_VERSION is officially released! Attach the binary found at ContentfulPersistence.framework.zip to the release on Github"
 
